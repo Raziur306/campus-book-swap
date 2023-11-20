e8264601d10c1c01bdea3fd0516b3dcb3e28f1e0
Md Raziur Rahaman Ronju <raziur.rahaman.ronju@gmail.com>
2023-11-21 02:18:56 +0600

    cors added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 import { app } from "./config";
 import { userRouter } from "./routes";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.use("/api", userRouter);
 
