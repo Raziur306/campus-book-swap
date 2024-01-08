04ec340a75dad2f5437d23e9f9188f87eb6c3ffc
Md Raziur Rahaman Ronju <raziur.rahaman.ronju@gmail.com>
2024-01-08 22:57:35 +0600

    toast issue fixed

--- frontend/components/verifyEmail.page/EmailVerification.tsx
+++ frontend/components/verifyEmail.page/EmailVerification.tsx
@@ -15,30 +15,32 @@ const EmailVerification = () => {
   const [isEmailVerified, setIsEmailVerified] = useState(true);
 
   const verificationCall = async () => {
-    try {
-      const emailVerifier = () =>
-        fetch(`${BASE_URL}/verify-email/${userId}`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
+    if (userId) {
+      try {
+        const emailVerifier = () =>
+          fetch(`${BASE_URL}/verify-email/${userId}`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        await toast.promise(emailVerifier(), {
+          loading: <b>Verifying Email...</b>,
+          success: (res: any) => {
+            if (!res.ok) {
+              setIsEmailVerified(false);
+              throw new Error(res.json());
+            }
+            setIsEmailVerified(true);
+            router.push("/sign-in");
+            return <b>Verification Successful!</b>;
           },
+          error: (err) => <b>{"Something went wrong"}</b>,
         });
-      await toast.promise(emailVerifier(), {
-        loading: <b>Verifying Email...</b>,
-        success: (res: any) => {
-          if (!res.ok) {
-            setIsEmailVerified(false);
-            throw new Error(res.json());
-          }
-          setIsEmailVerified(true);
-          router.push("/sign-in");
-          return <b>Verification Successful!</b>;
-        },
-        error: (err) => <b>{"Something went wrong"}</b>,
-      });
-    } catch (error) {
-      setIsEmailVerified(false);
-      console.log(error);
+      } catch (error) {
+        setIsEmailVerified(false);
+        console.log(error);
+      }
     }
   };
 
